7c5b78bd199ca0724bcfaa8e7564c77f909dd520
Christian Pace <Christian Pace>
2017-03-31 13:42:16 +0200

    Added basic routing to main page

--- ExpenseAppFrontEnd/src/app.ts
+++ ExpenseAppFrontEnd/src/app.ts
@@ -1,9 +1,18 @@
 import {Router, RouterConfiguration} from 'aurelia-router';
 
 export class App {
-  
-  
+  router: Router;
+
   constructor(){
     
+  }
+
+  configureRounter(config: RouterConfiguration, router: Router){
+    config.title = "Expense Manager Application";
+    config.map([
+      { route: '', moduleId: 'mainPage', title: "Exspense App - Main Page", name: 'main'}
+    ]);
+
+    this.router = router;
   }
 }
